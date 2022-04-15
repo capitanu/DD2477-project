7636c800fc97c5788a09065f7e9489dcfdad1605
richydany@gmail.com <richydany@gmail.com>
2022-04-15 10:50:55 +0200

    Added  comments and TODO

--- data/helper/inv_index.py
+++ data/helper/inv_index.py
@@ -13,25 +13,53 @@ class InvIndex:
         self.run()
 
     def _add_entry(self, str_val, off_set, doc_id):
+        """
+        Generates the dictionary that can passsed to elastic search handler
+        :param str_val:
+        :type str_val:
+        :param off_set:
+        :type off_set:
+        :param doc_id:
+        :type doc_id:
+        :return:
+        :rtype:
+        """
+        # Checks if the word is already present
         if str_val in self.INV_INDEX:
             doc_id_flag = False
+            # Goes through list of dictionaries to find if doc_id  is already present
             for ix, dict_val in enumerate(self.INV_INDEX[str_val]):
                 if doc_id == dict_val['docId']:
                     doc_id_flag = True
+                    # Appends the off_set value to the list
                     if off_set not in self.INV_INDEX[str_val][ix]['offSetArray']:
                         self.INV_INDEX[str_val][ix]['offSetArray'].append(off_set)
                     break
+            # If doc id is not present, create a new entry
             if not doc_id_flag:
                 self.INV_INDEX[str_val].append({
                     "docId": doc_id,
                     "offSetArray": [off_set]
                 })
+        #  If the word has not already been encountered adds the following
         else:
             self.INV_INDEX[str_val] = [{"docId": doc_id,
                                         "offSetArray": [off_set]}]
 
     def _add_tf(self, doc_id, word):
+        """
+        Populates the term frequency of each word in the document
+        :param doc_id:
+        :type doc_id: int
+
+        :param word:
+        :type word: str
+
+        :return: Nothing
+        :rtype: None
+        """
         if doc_id in self.TF:
+            # If doc id is already present, checks if the word is already present
             if word in self.TF[doc_id]:
                 self.TF[doc_id][word] += 1
             else:
@@ -42,12 +70,18 @@ class InvIndex:
             }
 
     def run(self):
+        # Iterating though documents
         for tmp_ix, rec in enumerate(self.records):
             doc_id = rec['_id']
+            # TODO: Change the below regex rule as it does not do it perfectly!
+            # Splits the summary to token
             list_tmp = [_ for _ in re.split("\s|(?<!\d)[,!.](?!\d)", rec['_source']['summary']) if _ != '']
             for off_set, s in enumerate(list_tmp):
+                # Cleaning the string to contain the stemmed version of every word
                 cleaned_str = self.lem.lemmatize(s).lower()
+                # Adds the string to the INV_IND
                 self._add_entry(cleaned_str, off_set, doc_id)
+                # Counts the number of terms in each document
                 self._add_tf(doc_id, cleaned_str)
 
     def get_inv_index(self):
