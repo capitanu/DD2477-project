475793059b107a25a819c0c7ac8d29cec6b89e22
Calin Capitanu <calincapitanu@gmail.com>
2022-04-24 13:41:32 +0200

    Cleaned the project

--- src/main/java/com/ir22/booksrec/QueryType.java
+++
@@ -1,9 +0,0 @@
-package com.ir22.booksrec;
-
-/**
- *  The different types of search queries we are considering in
- *  the course. 
- */
-public enum QueryType {
-    INTERSECTION_QUERY, PHRASE_QUERY, RANKED_QUERY 
-}
--- src/main/java/com/ir22/booksrec/RankingType.java
+++
@@ -1,9 +0,0 @@
-package com.ir22.booksrec;
-
-/**
- *  The different ways of ranking search results we are considering in
- *  the course. 
- */
-public enum RankingType {
-    TF_IDF, PAGERANK, COMBINATION, HITS
-}
